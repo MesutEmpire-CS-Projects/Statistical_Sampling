d58c470748d34bc4dfcc028d735843c47e30b0cd
MesutEmpire <samwainaina01@gmail.com>
2023-02-21 20:01:22 +0300

    Perform Simple Random Sampling

--- main.r
+++ main.r
@@ -1,3 +1,16 @@
 # Get data
 movie_data <- read.csv("Best Movie by Year Netflix.csv")
 
+# Simple Random Sampling
+# SRS with replacement
+set.seed(1)  # Set seed for reproducibility
+sample_size <- 20
+srs_with_replacement <- movie_data[sample(nrow(movie_data), sample_size,replace = TRUE),]
+"WITH REPLACEMENT"
+srs_with_replacement
+
+# SRS without replacement
+srs_without_replacement <- movie_data[sample(nrow(movie_data), sample_size,replace = FALSE),]
+"WITHOUT REPLACEMENT"
+srs_without_replacement
+
